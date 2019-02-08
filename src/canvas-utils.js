3a6195f37814c09d2059ad20c52cf1b3fc935a31
Lingyu Zhou <zly@TRLDSC2.CHBOSTON.ORG>
2019-02-08 12:43:28 -0500

    daily update, minor change.

--- src/canvas-utils.js
+++ src/canvas-utils.js
@@ -1,17 +1,20 @@
-export function applyCanvasStyle(ctx,{lineWidth,setLineDash,font,strokeStyle,fillStyle}){
+export function applyCanvasStyle(ctx,{lineWidth,setLineDash,
+                                      strokeStyle,fillStyle,
+                                      font
+                                      }) {
   if (lineWidth !== null && lineWidth !== undefined) {
     ctx.lineWidth = lineWidth;
   }
   if (setLineDash !== null && setLineDash !== undefined) {
     ctx.setLineDash(setLineDash);
   }
-  if (font !== null && font !== undefined) {
-    ctx.font = font;
-  }
   if (strokeStyle !== null && strokeStyle !== undefined) {
     ctx.strokeStyle = strokeStyle;
   }
   if (fillStyle !== null && fillStyle !== undefined) {
     ctx.fillStyle = fillStyle;
+  }
+  if (font !== null && font !== undefined) {
+    ctx.font = font;
   }
 }
